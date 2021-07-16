f43941078df17b358ac6e2076c1362eab077f6be
temptitle <mykhailo.mel1122@gmail.com>
2021-07-16 23:53:15 +0300

    Initial Commit

---
+++ AlbumsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlbumsApp
+//
+//  Created by Mykhailo Melnychuk on 16.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
